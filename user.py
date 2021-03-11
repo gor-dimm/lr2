af77eb545ec72c579aff3074e380c37b6d0ffb00
gor-dimm <booo130502@gmail.com>
2021-03-11 13:02:34 +0300

    1

---
+++ user.py
@@ -0,0 +1,6 @@
+nameUser = input("What is your name? ")
+ageUser = input("How old are you? ")
+cityUser = input("Where are you live? ")
+print("This is {0}.".format(nameUser))
+print("It is {0}.".format(ageUser))
+print("(S)he live in {0}.".format(cityUser))
